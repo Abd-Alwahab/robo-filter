d6e1e46ef0ef6b2bab3fe7affb7c539c43550b2c
Abd Alwahab <bayanadam10@gmail.com>
2019-09-27 10:41:48 +0300

    Updates

---
+++ precache-manifest.eb4fdfb60b27db346c7e632323ec4491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a240e7ae363796e009ef",
+    "url": "/robo-filter/static/js/runtime~main.a240e7ae.js"
+  },
+  {
+    "revision": "b7f99f3c92609dd7517e",
+    "url": "/robo-filter/static/js/main.b7f99f3c.chunk.js"
+  },
+  {
+    "revision": "7581f1a50536413a7b1f",
+    "url": "/robo-filter/static/js/2.7581f1a5.chunk.js"
+  },
+  {
+    "revision": "b7f99f3c92609dd7517e",
+    "url": "/robo-filter/static/css/main.9988006c.chunk.css"
+  },
+  {
+    "revision": "f9070f9364d1e5cac1561540df254958",
+    "url": "/robo-filter/index.html"
+  }
+];
